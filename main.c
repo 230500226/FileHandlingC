077ff506cd2bb5f89bc4a4aabb876a8eac97c49e
230500226 <125464717+230500226@users.noreply.github.com>
2023-08-03 11:12:27 +0000

    ex1

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
-cmain
+#include <stdio.h>
+
+int main(){
+
+    FILE *Log;
+
+    Log = fopen("./Log.txt", "wt");
+
+    fprintf(Log, "1. This is test for fprintf hello world text file...\n");
+
+    fputs("2. This is test for fputs hello world text file...\n", Log);
+
+    fclose(Log);
+
+
+return 0;}
